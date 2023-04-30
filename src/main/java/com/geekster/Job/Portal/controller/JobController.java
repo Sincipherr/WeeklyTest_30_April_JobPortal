4a4c44cbf62b39d02a05905227dbb9890f1fab6a
prashanth <prashanthprashu911@gmail.com>
2023-04-30 16:23:41 +0530

    Job portal project weekly test 30th april

--- src/main/java/com/geekster/Job/Portal/controller/JobController.java
+++ src/main/java/com/geekster/Job/Portal/controller/JobController.java
@@ -2,6 +2,7 @@ package com.geekster.Job.Portal.controller;
 
 import com.geekster.Job.Portal.model.Job;
 import com.geekster.Job.Portal.service.JobService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,29 +14,29 @@ public class JobController {
     JobService js;
 
     @GetMapping(value = "/bytitle/{title}")
-    public List<Job> searchbytitle(@PathVariable String title){
+    public List<Job> searchbytitle(@Valid @PathVariable String title){
         return js.bytitle(title);
     }
     @GetMapping(value = "/locationbysalary/{location}/{salary}")
-    public List<Job> jobbylocation(@PathVariable String location,@PathVariable Double salary){
+    public List<Job> jobbylocation(@Valid @PathVariable String location,@PathVariable Double salary){
         return js.jobbysalary(location,salary);
     }
     @GetMapping(value = "/bylocation/{location}")
-    public List<Job> getbylocation(@PathVariable String location){
+    public List<Job> getbylocation(@Valid @PathVariable String location){
         return js.jobbylocation(location);
     }
     @PostMapping(value = "/addjob")
-    public String addjob(@RequestBody List<Job> jobList){
+    public String addjob(@Valid @RequestBody List<Job> jobList){
        return js.addjob(jobList);
     }
 
     @PutMapping(value = "/updatesalary/{salary}/{id}")
-    public void updatesalary(@PathVariable Double salary,@PathVariable Integer id){
+    public void updatesalary(@Valid @PathVariable Double salary,@PathVariable Integer id){
         js.updatesalary(salary,id);
     }
 
     @DeleteMapping(value = "/deletejob/{salary}")
-    public void deletejob(@PathVariable Double salary){
+    public void deletejob(@Valid @PathVariable Double salary){
         js.deletejob(salary);
     }
 }
