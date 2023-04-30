425f24961e375ba2a322942542c0e3243735c911
prashanth <prashanthprashu911@gmail.com>
2023-04-30 16:07:22 +0530

    Job portal project weekly test 30th april

--- src/main/java/com/geekster/Job/Portal/model/Job.java
+++ src/main/java/com/geekster/Job/Portal/model/Job.java
@@ -1,9 +1,13 @@
 package com.geekster.Job.Portal.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.time.LocalDateTime;
 
@@ -17,8 +21,11 @@ public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String description;
+    @NotBlank
     private String location;
     private Double salary;
     private String companyName;
